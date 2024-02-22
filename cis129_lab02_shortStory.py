617ba20ba789be649978229ea760711f36ad50d1
NghiaTran4 <160821130+NghiaTran4@users.noreply.github.com>
2024-02-21 23:25:11 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,2 @@
-print('Hello, World!')
+# Here is new branch 
+print('Hi my name is Nghia Tran. I'm will be 20 year old in next Arpil and I'm from Vietnam. I come USA when i was 15 years old. At this time my English just have few word. But now I have change but I want be more better')
